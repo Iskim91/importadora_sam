d8f6620c6c91d142d2a0e2c9658320bc3435d4d8
Isac Kim <jk724814@gmail.com>
2020-03-02 13:50:14 +0000

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
+=======
+  root to: 'pages#home'
+>>>>>>> parent of 81f2bb2... Rails new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
