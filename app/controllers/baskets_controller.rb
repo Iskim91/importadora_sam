c1dc1920b4f151e3ec3ae512a5127593818ff28a
Isac Kim <jk724814@gmail.com>
2020-03-09 17:53:55 +0000

    FEAT: fixed the emty basket issue

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -11,22 +11,23 @@ class BasketsController < ApplicationController
 
   def payment
     @basket = Basket.find(params[:id])
-    total = @basket.transactions.map { |transaction| transaction.total_cents }.sum
-    @basket.total_cents = total
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: [{
-        name: @basket.id,
-        # images: @clothe.photos.first.key,
-        amount: @basket.total_cents,
-        currency: 'usd',
-        quantity: 1
-      }],
-      success_url: basket_url(@basket),
-      cancel_url: basket_url(@basket)
-    )
-    @basket.update(checkout_session_id: session.id)
-    redirect_to new_basket_payment_path(@basket)
-    authorize @basket
+    @basket.total = @basket.transactions.map { |transaction| transaction.amount * transaction.quantity }.sum
+    if @basket.transactions.count > 0
+      session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        line_items: [{
+          name: @basket.id,
+          # images: @clothe.photos.first.key,
+          amount: @basket.total_cents,
+          currency: 'usd',
+          quantity: 1
+        }],
+        success_url: basket_url(@basket),
+        cancel_url: basket_url(@basket)
+      )
+      @basket.update(checkout_session_id: session.id)
+      redirect_to new_basket_payment_path(@basket)
+      authorize @basket
+    end
   end
 end
