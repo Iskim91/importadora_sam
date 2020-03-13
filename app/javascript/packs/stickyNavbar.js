c1bb309d38ab56da5e0decd287722200d3706362
Isac Kim <jk724814@gmail.com>
2020-03-13 13:45:04 +0000

    FEAT: added the home button and styled the devise forms

--- app/javascript/packs/stickyNavbar.js
+++ app/javascript/packs/stickyNavbar.js
@@ -1,12 +1,15 @@
 const stickyNavbar = () => {
   const navFirst = document.querySelector(".navbar1")
   const navSecond = document.querySelector(".navbar2")
+  const homeBtn = document.querySelector(".home-button")
   window.addEventListener( "scroll", () => {
     if (window.scrollY >= 30) {
       navFirst.classList.add("navbar-fixed1")
+      homeBtn.classList.add("home-button1")
     }
     else {
       navFirst.classList.remove("navbar-fixed1")
+      homeBtn.classList.remove("home-button1")
     }
   })
   window.addEventListener( "scroll", () => {
