7ca778d8930bdfce42c6775e0227cb991df1596f
Isac Kim <jk724814@gmail.com>
2020-03-20 22:07:57 +0000

    FEAT: modified the seed

--- app/models/color.rb
+++ app/models/color.rb
@@ -2,4 +2,6 @@ class Color < ApplicationRecord
   has_and_belongs_to_many :clothes
   validates :name, presence: true
   validates :hex, presence: true
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ Color.destroy_all
 puts "creating colors"
 
 
-  hex = %w[0047AB 95F9E3 138A36 FF4B3E FF8552 054A29 B689FF 5C2751 6457A6 76E5FC 4BC0D9 2F92AF 2F568F 3174A0 3B5249 34252F]
+  hex = %w[5B8078 E1D5CE B46E58 0047AB 95F9E3 138A36 FF4B3E FF8552 054A29 B689FF 5C2751 6457A6 76E5FC 4BC0D9 2F92AF 2F568F 3174A0 3B5249 34252F]
   i = 0
 hex.count.times do
   url = open("https://www.thecolorapi.com/id?hex=#{hex[i]}").read
@@ -35,25 +35,27 @@ end
 
 puts "Creating Clothes"
 colors_ids = Color.all.map { |c| c.id }
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Apple",
-    description: "this apple is very good",
+    name: "Comfy T-shirt",
+    description: "Light and strechable t-shirt",
     category: "T-Shirt",
     gender: "Men",
     price_cents: 950,
-    sku: "mountain_shirt"
+    sku: "t-shirt"
     )
   clothe.photos.attach(io: File.open('app/assets/images/t-shirt2.jpeg'), filename: 't-shirt.jpeg')
   clothe.photos.attach(io: File.open('app/assets/images/shirt.jpeg'), filename: 't-shirt.jpeg')
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Apple",
-    description: "this apple is very good",
+    name: "Simple and comfortable T-shirt",
+    description: "Plain color made out of a breathable material",
     category: "T-Shirt",
     gender: "Men",
     price_cents: 950,
@@ -64,12 +66,14 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Flower",
-    description: "Floral dress is very pretty",
+    name: "Flower Dress",
+    description: "Playful floral dress",
     category: "Dress",
     gender: "Women",
     price_cents: 2000,
@@ -81,9 +85,11 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
     name: "Comfy Jeans",
     description: "Nice fitted jeans for any season",
@@ -97,11 +103,13 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Comfy Jeans",
+    name: "Textured Jeans",
     description: "Nice fitted jeans for any season",
     category: "Pants",
     gender: "Women",
@@ -113,11 +121,13 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Sweater ",
+    name: "Warm and comfy sweater",
     description: "Nice and soft sweated for the winter",
     category: "Sweater",
     gender: "Men",
@@ -128,11 +138,13 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Comfy sweater",
+    name: "Stylish sweater",
     description: "Nice and soft sweated for the winter",
     category: "Sweater",
     gender: "Men",
@@ -143,11 +155,13 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Comfy sweater",
+    name: "Oversized sweater",
     description: "Nice and soft sweated for the winter",
     category: "Sweater",
     gender: "Men",
@@ -158,11 +172,13 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
-    name: "Comfy suit",
+    name: "Comfy and cozy suit",
     description: "Nice and soft sweated for the winter",
     category: "Sweater",
     gender: "Men",
@@ -173,9 +189,11 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
     name: "Comfy sweater",
     description: "Nice and soft sweated for the winter",
@@ -188,9 +206,11 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
-2.times do
+1.times do
   clothe = Clothe.create!(
     name: "Comfy sweater",
     description: "Nice and soft sweated for the winter",
@@ -203,6 +223,8 @@ end
   rand(3..6).times do
     clothe.colors << Color.find(colors_ids.sample)
   end
+  detail = Detail.create( clothe: clothe, information: "Nice outfit in different colors.\r\n\r\nIt is 100% cotton.\r\n\r\nThe materials in this piece are from the best quality\r\n\r\nIt is extremely comfortable and breathable\r\n\r\nThe elasticity will provide full range of movement\r\n")
+  detail.photo.attach(io: File.open('app/assets/images/cotton.jpeg'), filename: 'cotton.jpeg')
 end
 
 
